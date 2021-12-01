13265f9014fd1fcd57bc66b52bf56e39bd466324
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-01 21:12:12 +0200

    # Add navbar with logo to chat room

--- chat-room/src/components/chat-room/ChatRoom.js
+++ chat-room/src/components/chat-room/ChatRoom.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import '../../styles/chat-room/NavBar.css';
+import logo from '../../images/logo.png';
 
 export default function ChatRoom() {
   return (
     <div>
-      <h1>chat room</h1>
+      {/* NAV BAR - TODO NAVBAR COMPONENT */}
+      <ul>
+        <li>
+          <img className='logo' alt='logo' src={logo} />
+        </li>
+        <li className='logout-nav'>
+          <i className='fas fa-sign-out-alt'></i> LogOut
+        </li>
+      </ul>
     </div>
   );
 }
