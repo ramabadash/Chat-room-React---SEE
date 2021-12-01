016b8fe69e432eec0e791187b70aca3b10bf8e2f
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-01 16:13:51 +0200

    # Create app component

--- chat-room/src/index.js
+++ chat-room/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from '../src/components/App.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello</h1>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
