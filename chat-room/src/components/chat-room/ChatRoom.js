c64af51c6ffdb12779a2163128b6a5255decade0
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-01 23:23:28 +0200

    # Basic grid template for chat-room components

--- chat-room/src/components/chat-room/ChatRoom.js
+++ chat-room/src/components/chat-room/ChatRoom.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../styles/chat-room/NavBar.css';
+import '../../styles/chat-room/ChatRoom.css';
 import logo from '../../images/logo.png';
 
 export default function ChatRoom() {
@@ -14,6 +15,25 @@ export default function ChatRoom() {
           <i className='fas fa-sign-out-alt'></i> LogOut
         </li>
       </ul>
+      {/* GENERAL SCREEN */}
+      <div className='chat-room'>
+        <div className='messages-list'>
+          {/* MESSAGES LIST - TODO- ADD COMPONENT */}
+          <div>message</div> {/* TODO - MESSAGE COMPONENT */}
+          <div>message</div> {/* TODO - MESSAGE COMPONENT */}
+          <div>message</div> {/* TODO - MESSAGE COMPONENT */}
+        </div>
+        <div className='message-input'>
+          {/* MESSAGE INPUT - TODO- ADD COMPONENT */}
+          <input type='text' /> <button>send</button>
+        </div>
+        <div className='members-list'>
+          {/* MEMBERS LIST - TODO- ADD COMPONENT */}
+          <div>MEMBER</div> {/* TODO - MEMBER COMPONENT */}
+          <div>MEMBER</div> {/* TODO - MEMBER COMPONENT */}
+          <div>MEMBER</div> {/* TODO - MEMBER COMPONENT */}
+        </div>
+      </div>
     </div>
   );
 }
