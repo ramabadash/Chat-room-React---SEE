2caea4b5f07607c05548384520c3de38e69cc3cd
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-02 14:30:40 +0200

    # Transfer "demoData" to parent component - "ChatRoom" instead of "MessagesList"

--- chat-room/src/components/chat-room/ChatRoom.js
+++ chat-room/src/components/chat-room/ChatRoom.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import moment from 'moment';
 import '../../styles/chat-room/NavBar.css';
 import '../../styles/chat-room/ChatRoom.css';
 import logo from '../../images/logo.png';
 import MessageInput from './MessageInput';
 import MessagesList from './MessagesList';
+import MemberList from './MemberList';
+
+// TODO - get messages array from the server
+const demoData = [
+  { author: 'rama', message: 'Hello', time: moment().format('llll') },
+  { author: 'bla', message: 'hello to you', time: moment().format('llll') },
+  { author: 'no one', message: 'I am no one', time: moment().format('llll') },
+  { author: 'no one2', message: 'I am no one2', time: moment().format('llll') },
+  {
+    author: 'rama',
+    message: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+    time: moment().format('llll'),
+  },
+];
 
 export default function ChatRoom() {
   return (
@@ -20,16 +35,14 @@ export default function ChatRoom() {
       {/* GENERAL SCREEN */}
       <div className='chat-room'>
         <div className='messages-list'>
-          <MessagesList />
+          <MessagesList chatData={demoData} />
         </div>
         <div className='message-input'>
           <MessageInput />
         </div>
         <div className='members-list'>
-          {/* MEMBERS LIST - TODO- ADD COMPONENT */}
-          <div>MEMBER</div> {/* TODO - MEMBER COMPONENT */}
-          <div>MEMBER</div> {/* TODO - MEMBER COMPONENT */}
-          <div>MEMBER</div> {/* TODO - MEMBER COMPONENT */}
+          <h2>Members: </h2>
+          <MemberList chatData={demoData} />
         </div>
       </div>
     </div>
--- chat-room/src/components/chat-room/MessagesList.js
+++ chat-room/src/components/chat-room/MessagesList.js
@@ -1,23 +1,9 @@
 import React, { useState } from 'react';
-import moment from 'moment';
 import Message from './Message';
 import '../../styles/chat-room/Message.css';
 
-// TODO - get messages array from the server
-const demoMessages = [
-  { author: 'rama', message: 'Hello', time: moment().format('llll') },
-  { author: 'bla', message: 'hello to you', time: moment().format('llll') },
-  { author: 'no one', message: 'I am no one', time: moment().format('llll') },
-  { author: 'no one2', message: 'I am no one2', time: moment().format('llll') },
-  {
-    author: 'rama',
-    message: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
-    time: moment().format('llll'),
-  },
-];
-
-export default function MessagesList() {
-  const [messages, setMessages] = useState(demoMessages);
+export default function MessagesList({ chatData }) {
+  const [messages, setMessages] = useState(chatData);
   return (
     <ul id='chat'>
       {messages.map(({ author, message, time }) => (
