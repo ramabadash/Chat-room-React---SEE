227dbd0570a95012a7118143fe1e284db57d3b97
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-03 11:50:04 +0200

    # Back  - Login functionality. Create token + refresh token to a user

--- chat-room/server/controller/users.js
+++ chat-room/server/controller/users.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const Token = require('../models/Token.js');
 
 /***** FUNCTIONS ******/
 // Register by -userName, email and password
@@ -19,6 +21,39 @@ exports.register = async (req, res, next) => {
     });
 
     res.send('Registered');
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Login by userName and password - create token + refresh token, save to DB and send back
+exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    if (!user) throw { status: 400, message: 'No such username' };
+    if (!bcrypt.compare(password, user.password)) throw { status: 400, message: 'Bad password' };
+
+    const userId = user._id;
+
+    const accessToken = jwt.sign({ username, userId }, process.env.ACCESS_SECRET, {
+      expiresIn: '1h',
+    });
+
+    const refreshToken = jwt.sign({ userId, username }, process.env.REFRESH_SECRET, {
+      expiresIn: '24h',
+    });
+
+    const tokenUser = await Token.findOne({ userId });
+    if (tokenUser) {
+      await Token.findOneAndUpdate({ userId }, { $set: { AccessToken: accessToken, RefreshToken: refreshToken } });
+    } else {
+      Token.create({ userId, AccessToken: accessToken, RefreshToken: refreshToken });
+    }
+
+    res.send({ accessToken, refreshToken });
   } catch (err) {
     next(err);
   }
--- chat-room/server/routers/usersRouter.js
+++ chat-room/server/routers/usersRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../controller/users.js');
+const { register, login } = require('../controller/users.js');
 
 // localhost:8080/users
 
 router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
