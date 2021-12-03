faff8713a3d6a53e00c20128f0a060cfa56904fd
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-03 12:00:04 +0200

    # Front - add "login" functionality to form - No use in the access token yet

--- chat-room/src/components/Login.js
+++ chat-room/src/components/Login.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
 import '../styles/Login.css';
+import { BASEURL } from '../components/App.js';
 
 export default function Login() {
+  /***** REFS *****/
+  const userNameInput = useRef('');
+  const passwordInput = useRef('');
+
+  /***** FUNCTIONS *****/
+  const navigate = useNavigate();
+
+  // Send login form
+  const login = async () => {
+    const userName = userNameInput.current.value;
+    const password = passwordInput.current.value;
+
+    try {
+      // TODO ADD VALIDATION
+      if (!userName || !password) {
+        throw new Error({ status: 400, message: 'Missing details' });
+      }
+      const response = await axios.post(`${BASEURL}/users/login`, {
+        userName,
+        password: String(password),
+      });
+      console.log(response.data); //TODO - ADD TOKEN TO COOKIES
+      return response.data; //TODO- ADD SUCCESS MESSAGE
+    } catch (error) {
+      console.log(error); //TODO- ADD ERROR MESSAGE
+    }
+  };
+
   return (
     <div className='login-form'>
       <img
@@ -15,17 +46,26 @@ export default function Login() {
         <span className='icon-span'>
           <i className='far fa-user'></i>
         </span>
-        <input id='userName' type='text' placeholder='User name'></input> {/* TODO ADD CONDITIONS */}
+        <input ref={userNameInput} id='userName' type='text' placeholder='User name'></input>{' '}
+        {/* TODO ADD CONDITIONS */}
       </div>
       <div>
         <span className='icon-span'>
           <i className='fas fa-unlock-alt'></i>
         </span>
-        <input id='password' type='password' placeholder='Password'></input> {/* TODO ADD CONDITIONS */}
+        <input ref={passwordInput} id='password' type='password' placeholder='Password'></input>{' '}
+        {/* TODO ADD CONDITIONS */}
       </div>
-      <Link to={'/home'}>
-        <button id='login-btn'>LOGIN!</button>
-      </Link>
+      <button
+        id='login-btn'
+        onClick={async () => {
+          const answer = await login();
+          if (answer) navigate('/home');
+          else return; //TODO- ADD ERROR FUNCTIONALITY / GET NEW TOKEN
+        }}
+      >
+        LOGIN!
+      </button>
       <hr />
       <Link to={'/register'}>
         <span>Not a member? create account!</span>
