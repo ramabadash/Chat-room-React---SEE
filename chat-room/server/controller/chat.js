8b305cc651e838039cf010f5d5c1a35945025c81
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-03 14:28:12 +0200

    # Back - get all messages - stream connection endpoint

--- chat-room/server/controller/chat.js
+++ chat-room/server/controller/chat.js
@@ -1,4 +1,5 @@
 const Message = require('../models/Message');
+const clients = [];
 
 // Save message to DB
 exports.sendMessage = async (req, res, next) => {
@@ -16,3 +17,30 @@ exports.sendMessage = async (req, res, next) => {
   }
 };
 
+// Get All Messages
+exports.getAllMessages = async (req, res, next) => {
+  try {
+    const { userName } = req.params;
+
+    const messageList = await Message.find({});
+    res.set({
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      Connection: 'keep-alive',
+
+      // enabling CORS
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    });
+    res.write(`data: ${JSON.stringify(messageList)}\n\n`);
+
+    // Save response to be answered
+    const newClient = {
+      userName,
+      res,
+    };
+    clients.push(newClient);
+  } catch (error) {
+    next(error);
+  }
+};
--- chat-room/server/routers/chatRouter.js
+++ chat-room/server/routers/chatRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { sendMessage } = require('../controller/chat.js');
+const { sendMessage, getAllMessages } = require('../controller/chat.js');
 
 // localhost:8080/chat
 
 router.post('/send-message', sendMessage); //
+router.get('/get-all', getAllMessages); //
 
 module.exports = router;
