d99bc86f916448d9a09a69e47de2bf5830590e78
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-03 11:49:58 +0200

    # Create token scema

---
+++ chat-room/server/models/Token.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const TokenSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  AccessToken: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  RefreshToken: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+const Token = mongoose.model('Token', TokenSchema);
+
+module.exports = Token;
